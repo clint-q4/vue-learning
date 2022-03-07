1b2b066d6b680b40b106975f5bd49dcc7e6334fc
Clinton <80900245+pravton@users.noreply.github.com>
2022-03-07 16:59:40 -0500

    Using vue components

---
+++ flashcard-app/src/components/DeckName.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{name}}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'DeckName',
+    props: ['name']
+  }
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+</style>
